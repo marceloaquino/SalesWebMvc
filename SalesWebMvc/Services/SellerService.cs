ae5e2494b895d5f34d9ef935d55f9b861d00d0af
Marcelo Aquino <macloureiro@outlook.com>
2020-06-11 14:07:06 -0300

    Simple Create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Services
@@ -19,5 +18,11 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
     }
 }
